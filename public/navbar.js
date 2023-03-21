a3e1c535815433192661a726c50ddbdaa890cb2f
Keith Gaines <keithg@celerocommerce.com>
2023-03-21 09:13:44 -0500

    fixed navbar transition

--- public/navbar.js
+++ public/navbar.js
@@ -1,15 +1,18 @@
 var burger = document.querySelector('.burger');
 var overlay = document.querySelector('#overlay');
 var parent_overlay = document.querySelector('body');
-parent_overlay.removeChild(overlay);
 
 burger.addEventListener('click', () => {
     if (burger.classList.contains('active_close')) {
-        parent_overlay.removeChild(overlay);
+        if (overlay) {
+            parent_overlay.removeChild(overlay);
+        }
         burger.classList.toggle('active_close');
         overlay.classList.toggle('active');
     } else {
-        parent_overlay.appendChild(overlay);
+        if (overlay) {
+            parent_overlay.appendChild(overlay);
+        }
         burger.classList.toggle('active_close');
         overlay.classList.toggle('active');
     }
