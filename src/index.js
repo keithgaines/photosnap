25f03bf577ef7c74a7a2110c8f14ab861496a7e3
Keith Gaines <keithg@celerocommerce.com>
2023-03-23 11:10:06 -0500

    edited layout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './index.css';
 import ImageComponent from './ImageCard';
 import StoriesImage from './StoriesImage.jsx';
 
